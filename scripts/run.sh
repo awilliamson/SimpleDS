cbc6388f838271b329e43a2e8aa493daf6002001
Ashley Williamson <11356993@students.lincoln.ac.uk>
2016-07-15 23:48:50 +0100

    Suppress pushd & popd echo, pipe to /dev/null

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,21 +10,21 @@ stty cols 120
 # Parent/Root directory relative to this script.
 # Allows this to be called from any working directoy and still function.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
-pushd $DIR
+pushd $DIR > /dev/null
 
 if [ "$1" = "train" ] ; then
    xterm -geometry 120x50 -T "SimpleDS.Server" -hold -e "ant SimpleDS" &
    sleep 2
-   pushd $DIR/web/main/
+   pushd $DIR/web/main/ > /dev/null
    xterm -geometry 80x20 -T "SimpleDS.Client" -hold -e "node runclient.js train"
-   popd
+   popd > /dev/null
 
 elif [ "$1" = "test" ] ; then 
    xterm -geometry 120x50 -T "SimpleDS.Client" -hold -e "ant SimpleDS" &
    sleep 2
-   pushd $DIR/web/main/
+   pushd $DIR/web/main/ > /dev/null
    xterm -geometry 80x20 -T "SimpleDS.Client" -hold -e "node runclient.js test"
-   popd
+   popd > /dev/null
 
 else 
    echo "usage: run.sh (train | test)" 
