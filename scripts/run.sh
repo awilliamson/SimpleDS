27297350fcf22a0fe828e226a5d9d75a2ab21170
Ashley Williamson <11356993@students.lincoln.ac.uk>
2016-07-15 23:41:46 +0100

    Allow run script to be invokation dir independent
    Use of pushd and popd to change directory so that the run script can be
    called from any working directory, and still function as expected.

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,21 +7,27 @@
 
 stty cols 120
 
+# Parent/Root directory relative to this script.
+# Allows this to be called from any working directoy and still function.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+pushd $DIR
+
 if [ $1 = "train" ] ; then
    xterm -geometry 120x50 -T "SimpleDS.Server" -hold -e "ant SimpleDS" &
    sleep 2
-   cd web/main
+   pushd $DIR/web/main/
    xterm -geometry 80x20 -T "SimpleDS.Client" -hold -e "node runclient.js train"
-   cd ../../
+   popd
 
 elif [ $1 = "test" ] ; then 
    xterm -geometry 120x50 -T "SimpleDS.Client" -hold -e "ant SimpleDS" &
    sleep 2
-   cd web/main
+   pushd $DIR/web/main/
    xterm -geometry 80x20 -T "SimpleDS.Client" -hold -e "node runclient.js test"
-   cd ../../
+   popd
 
 else 
    echo "usage: run.sh (train | test)" 
-fi 
+fi
 
+popd
